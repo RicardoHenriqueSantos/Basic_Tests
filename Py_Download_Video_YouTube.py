0a961e920de61883cdde8b2a3dc4387503591ea3
RicardoHenriqueSantos <ricardo_santins@live.com>
2022-11-22 22:14:58 -0400

    ADD

---
+++ Py_Download_Video_YouTube.py
@@ -0,0 +1,33 @@
+from pytube import YouTube
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+
+# COMANDOS INICIAIS
+root = tk.Tk()
+root.withdraw()
+
+
+# FUNÇÃO PARA RECEBER OS PARÂMETROS
+def informacoes(title, prompt):
+    result = simpledialog.askstring(title=title, prompt=prompt)
+    return result
+
+
+# INSERINDO A URL
+link_video = informacoes("Video Link", "INSIRA A URL: ")
+yt = YouTube(link_video)
+
+# INFORMAÇÕES SOBRE O VÍDEO
+print(f"CANAL: {yt.author}")
+print(f"TÍTULO DO VÍDEO: {yt.title}")
+print(f"VIEWS: {yt.views}")
+
+# DEFININDO A RESOLUÇÃO
+vd = yt.streams.get_highest_resolution()
+
+# INFORMANDO O CAMINHO ONDE SER SALVO O ARQUIVO
+caminho = r"C:"
+vd.download(caminho)
+
+# MENSAGEM DE CONCLUSÃO DO PROCESSO E ONDE ESTÁ SALVO O VÍDEO
+messagebox.showinfo("Mensagem", f"CONCLUÍDO! SALVO EM {caminho}")
