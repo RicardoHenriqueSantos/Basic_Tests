20a057c43e917e02327ab885285d479167a48633
RicardoHenriqueSantos <ricardo_santins@live.com>
2023-01-04 13:07:35 -0400

    ADD

---
+++ Py_Info_PC.py
@@ -0,0 +1,13 @@
+import wmi
+
+c = wmi.WMI()
+my_system = c.Win32_ComputerSystem()[0]
+
+print(f"Nome: {my_system.Name}")
+print(f"Marca: {my_system.Manufacturer}")
+print(f"Modelo: {my_system.Model}")
+print(f"Família: {my_system.SystemFamily}")
+print(f"Arquitetura: {my_system.SystemType}")
+print(f"Qtde. CPUs: {my_system.NumberOfProcessors}")
+
+
